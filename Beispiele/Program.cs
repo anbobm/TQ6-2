a53f4f9882c6d2add2889dd93a8b87a9c30d2ee4
Andreas <foo@bar.com>
2026-06-10 14:12:58 +0200

    Beispiel Variablen zugefügt

--- Beispiele/Program.cs
+++ Beispiele/Program.cs
@@ -2,7 +2,27 @@
 {
     private static void Main(string[] args)
     {
-        HelloWorld();
+        // HelloWorld();
+        // Variablen();
+
+    }
+
+    private static void Variablen()
+    {
+        // Deklaration kann separat (von Zuweisung) passieren:
+        string name;
+
+        // Zuweisung
+        name = "Max";
+
+        // Zuweisung nur mit richtigem Typ möglich (statisch Typisierte Programmiersprache)
+        // name = 3;
+
+        // Deklaration und Zuweisung in einem Schritt
+        int zahl = 3;
+
+        // Impliziter Typ mit Keyword var (geht nur bei gleichzeitiger Initialisierung)
+        var foo = 10.1;
     }
 
     private static void HelloWorld()
