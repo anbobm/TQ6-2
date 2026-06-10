7b167d01bc920782d08701190cf18a5435fb07c0
Andreas <foo@bar.com>
2026-06-10 14:03:22 +0200

    Beispielprojekt erstellt

---
+++ Beispiele/Program.cs
@@ -0,0 +1,12 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HelloWorld();
+    }
+
+    private static void HelloWorld()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
