2d01d2c05f69028aff9a7d5c2df945fad84cbeb1
Seb-He <257967715+Seb-He@users.noreply.github.com>
2026-06-11 15:58:21 +0200

    Hausaufgabe 11.06.

---
+++ Beispiele/Hausaufgabe 11.06.2026.cs
@@ -0,0 +1,4 @@
+int a = 20;
+int b = 10;
+int c = a * a + b * b;
+Console.WriteLine(Math.Sqrt(c));
--- Beispiele/Program2.cs
+++
@@ -1,8 +0,0 @@
-internal class Program
-{
-    private static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Das ist ein Test.");
-    }
-}
