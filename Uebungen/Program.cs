9e9290977603e12a526e5047c0f47484e8d9c8d1
katjaarens <257968712+katjaarens@users.noreply.github.com>
2026-06-11 07:02:55 +0200

    Test

---
+++ Uebungen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string firstName = "Bob";
+        string message = "Hello " + firstName;
+        Console.WriteLine(message);
+    }
+}
+
