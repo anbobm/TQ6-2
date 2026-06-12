7626700786121cdb102b708d0d09df6b1e6a60b9
ihornosach <257969028+ihornosach@users.noreply.github.com>
2026-06-12 16:11:30 +0200

    Die Loesung von 12.06.2026 hinzugefuegt

---
+++ Hausaufgaben/20260612_Aufgabe/csharp_loesung.cs
@@ -0,0 +1,47 @@
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int note;
+        do
+        {
+            System.Console.Write("Geben Sie die Note ein: ");
+            note = int.Parse(Console.ReadLine()); 
+            Ergebniss(note);
+        }while(note != 0);
+    }
+
+    private static void Ergebniss(int note)
+    {
+        switch (note)
+        {
+            case > 0 and < 30:
+                Console.WriteLine("ungenügend");
+                break;
+
+            case > 30 and < 50:
+                Console.WriteLine("mangelhaft");
+                break;
+
+            case >= 50 and <= 66:
+                Console.WriteLine("ausreichend");
+                break;
+
+            case >= 67 and <= 80:
+                Console.WriteLine("befriedigend");
+                break;
+
+            case > 80 and <= 91:
+                Console.WriteLine("gut");
+                break;
+
+            case >= 92 and <= 100:
+                Console.WriteLine("sehr gut");
+                break;
+
+            default:
+                Console.WriteLine("Ungültige Punktzahl");
+                break;
+        }
+    }
+}
