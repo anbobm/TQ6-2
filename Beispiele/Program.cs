8b9f087058eb53840f72088b6a81cbecd81fd3a7
Andreas <foo@bar.com>
2026-06-12 13:56:09 +0200

    Beispiellösung Hausaufgabe zugefügt

--- Beispiele/Program.cs
+++ Beispiele/Program.cs
@@ -8,7 +8,35 @@
         // TypUmwandlungen();
         // ArithmetischeOperatoren();
         // VergleichsOperatoren();
-        LogischeOperatoren();
+        // LogischeOperatoren();
+        ErsteAufgabe();
+    }
+
+    // Aufgabe 1 vom 11.06.
+    static void ErsteAufgabe()
+    {
+        Console.Write("Geben Sie die erste Zahl ein: ");
+        int numberOne = int.Parse(Console.ReadLine()!);
+ 
+        Console.Write("Geben Sie die zweite Zahl ein: ");
+        int numberTwo = int.Parse(Console.ReadLine()!);
+ 
+        int sum = numberOne + numberTwo;
+        Console.WriteLine($"Die Summe von {numberOne} + {numberTwo} = {sum}");
+    }
+ 
+    // Aufgabe 2 vom 11.06.
+    static void ZweiteAufgabe()
+    {
+        Console.Write("Geben Sie 'a' ein: ");
+        int a = int.Parse(Console.ReadLine()!);
+ 
+        Console.Write("Geben Sie 'b' ein: ");
+        int b = int.Parse(Console.ReadLine()!);
+ 
+        var c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+ 
+        Console.WriteLine($"c = √a2 + b2 = {c}");
     }
 
     private static void LogischeOperatoren()
@@ -146,4 +174,9 @@
     {
         Console.WriteLine("Hello, World!");
     }
+}
+
+class Foo
+{
+    public void Bar() {}
 }
