e6a4215870881258dc4e221eaa2f3fd0df93b6bf
Andreas <foo@bar.com>
2026-07-15 15:16:45 +0200

    Beispiel Tests zugefügt

---
+++ Kurstage/2026-07-15/BeispielTests/TemperatursensorTests.cs
@@ -0,0 +1,47 @@
+﻿namespace BeispielTests;
+
+public class TemperatursensorTests
+{
+    [Fact]
+    public void Erhoehen_TemperaturUmWertErhoehenErfolgreich()
+    {
+        // Arrange
+        var sensor = new Temperatursensor();
+
+        // Act
+        sensor.Erhoehen(100);
+
+        // Assert
+        Assert.Equal(100, sensor.TemperaturCelsius);
+    }
+
+    [Fact]
+    public void Senken_TemperaturUmWertSenkenErfolgreich()
+    {
+        var sensor = new Temperatursensor();
+
+        sensor.Senken(50);
+
+        Assert.Equal(-50, sensor.TemperaturCelsius);
+    }
+
+    [Fact]
+    public void Senken_TemperaturUnterAbsoluterNullpunktWirftException()
+    {
+        var sensor = new Temperatursensor();
+
+        Assert.Throws<ArgumentException>(() => sensor.Senken(273.151m));
+    }
+
+    [Fact]
+    public void TemperaturFahrenheit()
+    {
+        var sensor = new Temperatursensor();
+
+        sensor.TemperaturCelsius = 0;
+        Assert.Equal(32, sensor.TemperaturFahrenheit);
+
+        sensor.TemperaturCelsius = 100;
+        Assert.Equal(212, sensor.TemperaturFahrenheit);
+    }
+}
