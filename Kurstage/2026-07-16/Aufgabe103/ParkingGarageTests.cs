3d08b0e9cd47d1e666d6865ac88752d4b46ead93
Andreas <foo@bar.com>
2026-07-16 15:54:24 +0200

    Lösung zugefügt

---
+++ Kurstage/2026-07-16/Aufgabe103/ParkingGarageTests.cs
@@ -0,0 +1,58 @@
+﻿namespace Aufgabe103;
+
+public class ParkingGarageTests
+{
+    [Fact]
+    public void ParkingGarage_NachInitialisierungFreiePlätze()
+    {
+        var garage = new ParkingGarage(10);
+
+        Assert.Equal(10, garage.FreeSpaces);
+    }
+
+    [Fact]
+    public void Park_WithFreeSpacesLeft_ReturnsTrueAndReducesFreeSpaces()
+    {
+        var garage = new ParkingGarage(10);
+
+        var success = garage.Park();
+
+        Assert.True(success);
+        Assert.Equal(9, garage.FreeSpaces);
+    }
+
+    [Fact]
+    public void Park_NoFreeSpacesLeft_ReturnsFalseAndFreeSpacesRemainsZero()
+    {
+        var garage = new ParkingGarage(1);
+        garage.Park();
+
+        var success = garage.Park();
+
+        Assert.False(success);
+        Assert.Equal(0, garage.FreeSpaces);
+    }
+
+    [Fact]
+    public void Leave_NonEmptyGarageReturnsTrueAndIncreasesFreeSpaces()
+    {
+        var garage = new ParkingGarage(10);
+        garage.Park();
+
+        var success = garage.Leave();
+
+        Assert.True(success);
+        Assert.Equal(10, garage.FreeSpaces);
+    }
+
+    [Fact]
+    public void Leave_EmptyGarageReturnsFalseAndFreeSpacesRemain()
+    {
+        var garage = new ParkingGarage(10);
+
+        var success = garage.Leave();
+
+        Assert.False(success);
+        Assert.Equal(10, garage.FreeSpaces);
+    }
+}
