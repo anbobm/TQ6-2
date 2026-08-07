5487d5ad681eb0e9d6a0cac0dd2f2d4adf5294cc
Andreas <foo@bar.com>
2026-08-07 13:36:08 +0200

    Lösung ergänzt

--- Kurstage/2026-08-06/Aufgabe115/Pages/Index.cshtml.cs
+++ Kurstage/2026-08-06/Aufgabe115/Pages/Index.cshtml.cs
@@ -1,12 +1,27 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Aufgabe115.Pages;
 
 public class IndexModel : PageModel
 {
-    public void OnGet()
-    {
+    public string Begrüßung { get; set; }
 
+    public void OnGet(DateTime? geburtsdatum)
+    {
+        if (!geburtsdatum.HasValue)
+        {
+            return;
+        }
+
+        var heute = DateTime.Now;
+
+        if (geburtsdatum.Value.Month == heute.Month
+            && geburtsdatum.Value.Day == heute.Day)
+        {
+            Begrüßung = "Alles Gute zum Geburtstag!";
+        }
+        else {
+            Begrüßung = "Willkommen! (Sorry, keine Geschenke heute)";
+        } 
     }
 }
