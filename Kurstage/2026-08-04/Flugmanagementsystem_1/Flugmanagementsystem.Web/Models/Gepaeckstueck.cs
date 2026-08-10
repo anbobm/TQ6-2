750086f6da55cda491972914cf826de7550a35f4
natahariharinata-eng <257970419+natahariharinata-eng@userrs.noreply.github.com>
2026-08-10 11:06:33 +0200

    Add Gepaeckstueck model to Razor project

---
+++ Kurstage/2026-08-04/Flugmanagementsystem_1/Flugmanagementsystem.Web/Models/Gepaeckstueck.cs
@@ -0,0 +1,8 @@
+namespace Flugmanagementsystem.Web.Models;
+
+public class Gepaeckstueck
+{
+    public int GepaeckstueckId { get; set; }
+    public int BuchungId { get; set; }
+    public decimal Gewicht { get; set; }
+}
