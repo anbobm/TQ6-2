0fef447743a15460ea2a95da826336e5f7ae3e7d
Andreas <foo@bar.com>
2026-08-13 13:17:08 +0200

    Lösung zugefügt

---
+++ BiboApp/Pages/BuchDetail.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+public class BuchDetailModel : PageModel
+{
+    Db db;
+
+    public Buch Buch { get; set; }
+
+    public BuchDetailModel(Db db)
+    {
+        this.db = db;
+    }
+
+    public void OnGet(int id)
+    {
+        Buch = db.Bücher
+            .Include(buch => buch.Autor)
+            .Where(buch => buch.Id == id)
+            .FirstOrDefault();
+    }
+}
